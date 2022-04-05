c69d6bd5e0e6c8c758f340011754f3aeb2b8dbba
Shifra Wagshall <shifrawagshall@gmail.com>
2022-04-05 13:38:28 -0400

    Added project model and renamed so vars.

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -42,14 +42,14 @@ module.exports = {
     const token = signToken(user);
     res.json({ token, user });
   },
-  // save a book to a user's `savedBooks` field by adding it to the set (to prevent duplicates)
+  // save a project to a user's `savedprojects` field by adding it to the set (to prevent duplicates)
   // user comes from `req.user` created in the auth middleware function
-  async saveBook({ user, body }, res) {
+  async saveproject({ user, body }, res) {
     console.log(user);
     try {
       const updatedUser = await User.findOneAndUpdate(
         { _id: user._id },
-        { $addToSet: { savedBooks: body } },
+        { $addToSet: { savedprojects: body } },
         { new: true, runValidators: true }
       );
       return res.json(updatedUser);
@@ -58,11 +58,11 @@ module.exports = {
       return res.status(400).json(err);
     }
   },
-  // remove a book from `savedBooks`
-  async deleteBook({ user, params }, res) {
+  // remove a project from `savedprojects`
+  async deleteproject({ user, params }, res) {
     const updatedUser = await User.findOneAndUpdate(
       { _id: user._id },
-      { $pull: { savedBooks: { bookId: params.bookId } } },
+      { $pull: { savedprojects: { projectId: params.projectId } } },
       { new: true }
     );
     if (!updatedUser) {
